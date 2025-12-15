1c21799e95aa074e4e3e86fe6540b38464fcb61d
jonorex <jonofex@gmail.com>
2025-12-14 23:25:05 -0300

    interface para controle de animações

---
+++ Assets/Scripts/IAnimController.cs
@@ -0,0 +1,18 @@
+
+
+
+
+public interface IAnimController {
+    public void Attack();
+
+    public void Walk();
+
+    public void Run();
+
+    public void Death();
+
+    public void Idle();
+
+    public void GetDamage();
+
+}
