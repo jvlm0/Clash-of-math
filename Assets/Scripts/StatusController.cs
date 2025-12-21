bbe5c404cde7856f7ad7b83570fda880f5fe7b09
jonorex <jonofex@gmail.com>
2025-12-20 23:57:22 -0300

    script comum para a definição de status para todo o tipo de tropa (player, boss, npcs)

---
+++ Assets/Scripts/StatusController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class StatusController : MonoBehaviour
+{
+    
+    public float attackRange = 2.0f;
+
+    
+    public float damage = 10.0f;
+
+    
+    public float hp = 100.0f;
+
+    
+    public LayerMask targetLayer;
+}
